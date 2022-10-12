7467e21d2c1481d96665661892fe55ae0fe38217
Harl-min <99662720+Harl-min@users.noreply.github.com>
2022-10-12 16:47:22 +0100

    Updated

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,9 @@
-btns = document.getElementsByClassName("nav-item");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function () {
-			console.log(i);
-        });
-    }
+// btns = document.getElementsByClassName("nav-item");
+//     for (var i = 0; i < btns.length; i++) {
+//         btns[i].addEventListener("click", function () {
+// 			console.log(i);
+//         });
+//     }
       
+
+// alert ()
