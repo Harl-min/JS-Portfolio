141b4db7a947692a6ea553a9fa319f567017e627
Harl-min <99662720+Harl-min@users.noreply.github.com>
2022-10-21 17:04:05 +0100

    Update

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,3 +7,31 @@
       
 
 // alert ()
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+// var tablink = document.getElementsByClassName("tab-link");
+// var tabcontents = document.getElementsByClassName("carousel-wrapper");
+
+// function opentab(tabname) {
+//     for(tablink of tablink){
+//         tablink.classList.remove("active-click");
+//     }
+//     for(tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-card");
+//     }
+//     event.currentTarget.classList.add("active-click");
+//     document.getElementById(tabname).classList.add("active-card")
+// }
